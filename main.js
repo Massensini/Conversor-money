bce1403beee1a7cb3fe96798f780bf3ad96090ea
Gabriel Massensini <89725700+Massensini@users.noreply.github.com>
2022-11-28 23:22:17 -0300

    JS in progress

---
+++ main.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('convert-button')
+
+const dolar = 5.2
+
+const convertValues = () => {
+    const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+}
+
+button.addEventListener('click', convertValues)
